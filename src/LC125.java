7c9dd3a045683c9ac2d84376c225c529736e547a
Ru Zhang <rzhang85@asu.edu>
2020-08-03 22:06:20 -0700

    8/3 LC125

---
+++ src/LC125.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class LC125 {
+    public boolean isPalindrome(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) {
+                i++;
+            }
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) {
+                j--;
+            }
+
+            if (i < j && Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+        }
+
+        return true;
+    }
+}
